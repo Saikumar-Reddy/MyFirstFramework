81c3ee753bdb031de84686c11f0b2484c3202aeb
VenkatReddy <venkat.varra@gmail.com>
2017-04-12 16:49:17 +0530

    Initial Commit

---
+++ MyFirstFramework/MyFirstFramework.h
@@ -0,0 +1,19 @@
+//
+//  MyFirstFramework.h
+//  MyFirstFramework
+//
+//  Created by VenkatReddy on 4/12/17.
+//  Copyright © 2017 colors. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyFirstFramework.
+FOUNDATION_EXPORT double MyFirstFrameworkVersionNumber;
+
+//! Project version string for MyFirstFramework.
+FOUNDATION_EXPORT const unsigned char MyFirstFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyFirstFramework/PublicHeader.h>
+
+
