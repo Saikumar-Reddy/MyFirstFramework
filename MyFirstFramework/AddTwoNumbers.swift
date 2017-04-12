dd078f290872bb3adff57a27ca9d9f3fac300483
VenkatReddy <venkat.varra@gmail.com>
2017-04-12 18:01:38 +0530

    commit

---
+++ MyFirstFramework/AddTwoNumbers.swift
@@ -0,0 +1,18 @@
+//
+//  AddTwoNumbers.swift
+//  MyFirstFramework
+//
+//  Created by VenkatReddy on 4/12/17.
+//  Copyright © 2017 colors. All rights reserved.
+//
+
+import UIKit
+
+class AddTwoNumbers: NSObject {
+    public func sum(a:Int, ofTwoNumbers b:Int )-> Int{
+        let c = a + b
+        return c
+        
+    }
+
+}
