f260a71f03d2937674b8ef3e02b3cb9c402981c2
Sai Kumar Reddy <saikumarreddy.gujjula@gmail.com>
2023-06-30 11:06:19 +0530

    Update GetJsonData.swift

--- MyFirstFramework/GetJsonData.swift
+++ MyFirstFramework/GetJsonData.swift
@@ -1,18 +1,1 @@
-//
-//  GetJsonData.swift
-//  MyFirstFramework
-//
-//  Created by VenkatReddy on 4/12/17.
-//  Copyright © 2017 colors. All rights reserved.
-//
 
-import UIKit
-
-public class GetJsonData: NSObject {
-
-    public func sum(a:Int, ofTwoNumbers b:Int )-> Int{
-        let c = a + b
-        return c
-        
-    }
-}
